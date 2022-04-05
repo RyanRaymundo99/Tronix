102326e4ab1d4bfd81ae463a47ceb82ecc0d3913
Ryan Raymundo <90113061+RyanRaymundo99@users.noreply.github.com>
2022-04-05 15:56:31 -0700

    enter

--- api/index.js
+++ api/index.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const doteenv = require("dotenv");
+const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
-const userRoute = require("./routes/users")
+const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
 
-doteenv.config();
+dotenv.config();
 
 main().catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect(process.env.MONGO_URL);
-  }
+  await mongoose.connect(process.env.MONGO_URL);
+}
 
-  app.use("/api/auth", authRoute)
-  app.use("/api/users", userRoute)
+app.use(express.json());
 
-app.listen( 8800, ()=>{
-    console.log("hello")
-})
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
